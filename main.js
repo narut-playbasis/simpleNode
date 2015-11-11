b589a16cf0629d67c98970d35f83822a2f1eacbb
Narut Tripopnat <narut.t@playbasis.com>
2015-11-11 15:06:42 +0700

    check in first version of code

---
+++ main.js
@@ -0,0 +1,43 @@
+var redis = require('redis');
+var fs = require('fs')
+    , http = require('http')
+    , socketio = require('socket.io');
+var client = redis.createClient(6379,'127.0.0.1');
+
+client.on('connect', function() {
+    console.log('connected');
+});
+
+client.hmset('frameworks', 'javascript', 'AngularJS', 'css', 'Bootstrap', 'node', 'Express');
+
+client.hgetall('frameworks', function(err, object) {
+    console.log(object);
+});
+
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-type': 'text/html'});
+    res.end(fs.readFileSync(__dirname + '/index.html'));
+}).listen(8080, function() {
+    console.log('Listening at: http://localhost:8080');
+});
+socketio.listen(server).on('connection', function (socket) {
+    socket.on('message', function (msg) {
+        console.log('Message Received: ', msg);
+        socket.broadcast.emit('message', msg);
+    });
+});
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
